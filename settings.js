65db2a5f74bd4b59a43a0571034e3cedd6515f94
dlarudgus20 <dlarudgus20@naver.com>
2019-11-21 00:21:07 +0900

    settings.js validation

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const joi = require('@hapi/joi')
 
 const confFilePath = path.join(__dirname, 'etc/settings.json')
 
@@ -10,6 +11,37 @@ module.exports.get = function() {
         return settings
     }
 
-    settings = JSON.parse(fs.readFileSync(confFilePath))
-    return settings
+    try {
+        settings = JSON.parse(fs.readFileSync(confFilePath))
+
+        const schema = joi.object({
+            frontweb: joi.object({
+                port: joi.number().required().strict().integer().min(1),
+                proxy: joi.alternatives(
+                    joi.number().strict().integer().min(0),
+                    joi.string()).required().allow(null),
+                prefix: joi.string().required().allow(''),
+                session: joi.object({
+                    name: joi.string().required(),
+                    secret: joi.string().required()
+                }).required()
+            }).required(),
+            db: joi.object({
+                host: joi.string().required(),
+                user: joi.string().required(),
+                password: joi.string().required(),
+                database: joi.string().required()
+            }).required()
+        })
+        const result = schema.validate(settings)
+        if (result.error !== undefined) {
+            throw result.error
+        }
+
+        return settings
+    }
+    catch (e) {
+        console.log(`etc/settings.json is invalid: ${e.message}`)
+        process.exit(1)
+    }
 }
