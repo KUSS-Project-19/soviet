e84e623bc2a41d2e4186d3c90dd09e2298fe7ced
16박진석 <key13689@gmail.com>
2019-12-08 09:48:54 +0900

    is there an end?

--- frontweb.js
+++ frontweb.js
@@ -198,12 +198,6 @@ module.exports.createServer = createServer
 function createDeviceServer(callback) {
     const app = express()
 
-    let isProxy = false
-    if (settings.frontweb.proxy !== null) {
-        isProxy = true
-        app.set('trust proxy', settings.frontweb.proxy)
-    }
-
     const sessionStore = new devSQLiteStore({
         table: 'sessions',
         db: 'deviceSessionsDB.db',
